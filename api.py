2f491b370f6b2941ee7149c11c58f6d7e423d16e
havish10 <netlahavish@gmail.com>
2019-03-17 12:21:12 -0400

    f

---
+++ api.py
@@ -0,0 +1,42 @@
+import pickle
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, HashingVectorizer
+from sklearn.model_selection import train_test_split
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+def predict(text):
+    df = pd.read_csv('./fake_or_real_news.csv')
+
+    y = df.label
+    df = df.drop('label', axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(df['text'], y, test_size=0.33, random_state=53)
+
+    user_input = [text]
+    # Test Input
+    count_vectorizer_input = CountVectorizer(stop_words='english')
+    count_train = count_vectorizer_input.fit_transform(X_train)
+    count_input = count_vectorizer_input.transform(user_input)
+
+    # load the model from disk
+    filename = 'finalized_model.sav'
+    loaded_model = pickle.load(open(filename, 'rb'))
+    result = loaded_model.predict(count_input)
+    return result
+
+# print(predict("XDD"))
+
+
+@app.route("/")
+def hello():
+    text = request.args.get('text')
+    result = predict(text)
+    return str(result)
+
+if __name__ == "__main__":
+    app.run()
+
+
+
